118a11965104845809fdf2ee1a3266042386fe4a
Nantita Chomsiri <nantitakikk@gmail.com>
2021-05-02 21:31:16 +0700

    popup

---
+++ main.js
@@ -0,0 +1,15 @@
+const modal = document.getElementsByClassName('modal')[0];
+const modalBtn = document.getElementsByClassName('modalBtn');
+const closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+// modalBtn.addEventListener('click',openModal);
+// function openModal(){
+//   modal.style.display = "block";
+// }
+
+// closeBtn.addEventListener('click',closeModal);
+// function closeModal(){
+//   modal.style.display="none";
+// }
+
+console.log(modalBtn);
