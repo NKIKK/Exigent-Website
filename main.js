bc3369440622d71c03fe294355c6d24ee1858a25
Nantita Chomsiri <nantitakikk@gmail.com>
2021-05-02 23:39:20 +0700

    modal js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const modal = document.getElementsByClassName('modal')[0];
-const modalBtn = document.getElementsByClassName('modalBtn');
-const closeBtn = document.getElementsByClassName('closeBtn')[0];
-
+const modalBtn = document.querySelectorAll('.modalBtn');
+const closeBtn = document.querySelectorAll('.closeBtn');
+const microphone = document.querySelector('#microphone');
+const modalName = document.querySelector('#popupName');
+const modalSend = document.querySelector('#popupSend');
 // modalBtn.addEventListener('click',openModal);
 // function openModal(){
 //   modal.style.display = "block";
@@ -11,5 +12,35 @@ const closeBtn = document.getElementsByClassName('closeBtn')[0];
 // function closeModal(){
 //   modal.style.display="none";
 // }
+modalBtn.forEach(e => {
+  e.addEventListener('click',function (){
+      if(e.classList.contains('modalSendBtn'))
+      {
+        modalSend.style.display = "block";
 
-console.log(modalBtn);
+      }
+      else if(e.classList.contains('modalName'))
+      {
+        modalName.style.display="block";
+
+      }
+      else if(e.classList.contains('closeNameBtn'))
+      {
+        modalName.style.display="none";
+
+      }
+      else if(e.classList.contains('closeSendBtn'))
+      {
+        modalSend.style.display="none";
+
+      }// else if(e.classList.contains('idle'))
+      // {
+      //   microphone.classList.replace('idle','recording');
+      //   microphone.nextSibling.textContent="Recording\.\.\.
+      //   click again to finish";
+      // }
+     });
+});
+
+
+console.log(modalName);
